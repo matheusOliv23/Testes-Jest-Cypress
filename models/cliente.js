820bb3af8d228434b1dd64feb52d9fc8f3b064c9
matheusoliv23 <matheushenri1996@gmail.com>
2022-04-29 11:02:01 -0300

    ajustes

--- models/cliente.js
+++ models/cliente.js
@@ -16,15 +16,15 @@ module.exports = class Cliente {
   static todos() {
     return [
       new Cliente(1, "Matheus", "123456"),
-      new Cliente(2, "Matheus", "123456"),
-      new Cliente(3, "Matheus", "123456"),
-      new Cliente(4, "Matheus", "123456"),
-      new Cliente(5, "Matheus", "123456"),
-      new Cliente(6, "Matheus", "123456"),
-      new Cliente(7, "Matheus", "123456"),
-      new Cliente(8, "Matheus", "123456"),
-      new Cliente(9, "Matheus", "123456"),
-      new Cliente(10, "Matheus", "123456"),
+      new Cliente(2, "Caio", "123456"),
+      new Cliente(3, "Jose", "123456"),
+      new Cliente(4, "Maria", "123456"),
+      new Cliente(5, "Fernando", "123456"),
+      new Cliente(6, "Gustavo", "123456"),
+      new Cliente(7, "Carlos", "123456"),
+      new Cliente(8, "Augusto", "123456"),
+      new Cliente(9, "Luciane", "123456"),
+      new Cliente(10, "Roberto", "123456"),
     ];
   }
 };
